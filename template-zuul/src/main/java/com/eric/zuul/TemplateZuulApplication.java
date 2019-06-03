1ce9e115165cdfa6625ec4980a7bfb5cd0b44c67
EricShen <ahsbt@126.com>
2019-06-03 20:22:33 +0800

    Zuul

---
+++ template-zuul/src/main/java/com/eric/zuul/TemplateZuulApplication.java
@@ -0,0 +1,19 @@
+package com.eric.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 路由网关 Zuul
+ *
+ * @author EricShen
+ * @date 2910-06-03
+ */
+@SpringBootApplication
+public class TemplateZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TemplateZuulApplication.class, args);
+    }
+
+}
