f3abdde682546a877bd26ff8dd016a2dae843fdc
EricShen <ahsbt@126.com>
2019-06-03 15:55:45 +0800

    admin init

---
+++ template-admin/src/main/java/com/eric/admin/TemplateAdminApplication.java
@@ -0,0 +1,13 @@
+package com.eric.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplateAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TemplateAdminApplication.class, args);
+    }
+
+}
